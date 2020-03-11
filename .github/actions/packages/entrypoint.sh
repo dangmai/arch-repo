fbfa403aaa453dac8e1d825acfa09bc77eb1b1f7
Dang Mai <contact@dangmai.net>
2020-03-11 17:55:46 -0400

    Start building packages

---
+++ .github/actions/packages/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+pwd
+cd /home/builduser
+
+echo "Importing GPG keys"
+# Strip out comments before importing keys
+grep -o '^[^#]*' gpg_keys.txt | xargs -I '{}' gpg --recv-keys '{}'
+
+echo "Adding custom repository to Pacman configuration"
+mkdir repo
+repo-add repo/personal.db.tar
+sudo tee -a /etc/pacman.conf > /dev/null <<EOT
+[personal]
+SigLevel = Optional TrustAll
+Server = file:///home/builduser/repo
+EOT
+
+echo "Installing aurutils"
+sudo pacman --noconfirm -Syu git
+git clone https://aur.archlinux.org/aurutils.git
+cd aurutils
+gpg --recv-keys 6BC26A17B9B7018A # aurutils maintainer key
+makepkg --noconfirm -si
+
+echo "Use aurutils to sync packages"
+# Strip out comments before syncing pacakges
+grep -o '^[^#]*' gpg_keys.txt | xargs -I '{}' aur sync --noconfirm --noview '{}'
