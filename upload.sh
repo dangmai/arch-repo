cabc5b5661a8c51fb617b3c3d1a3a68a2b77c012
Dang Mai <contact@dangmai.net>
2020-03-14 18:21:26 -0400

    Upload directly to repo dir

--- upload.sh
+++ upload.sh
@@ -4,11 +4,7 @@ set -euxo pipefail
 
 # Uploading artifacts in the `repo` directory to a WebDav directory
 # that is secured by an access token
-curl -X DELETE -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-repo"
-curl --fail -X MKCOL -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-repo"
+curl -X MKCOL -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/repo"
 
 cd repo
-find . -type f -exec curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T {} "${WEBDAV_URL}/tmp-repo/"{} \;
-
-curl -X DELETE -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/repo"
-curl --fail -X MOVE -H "Authorization: Bearer ${WEBDAV_TOKEN}" -H "Destination:${WEBDAV_URL}/repo" "${WEBDAV_URL}/tmp-repo"
+find . -type f -exec curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T {} "${WEBDAV_URL}/repo/"{} \;
