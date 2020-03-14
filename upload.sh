f182c8a7aa008f9100522a3ae6acff6b478870ef
Dang Mai <contact@dangmai.net>
2020-03-14 18:10:02 -0400

    Fix find syntax

--- upload.sh
+++ upload.sh
@@ -8,7 +8,7 @@ curl -X DELETE -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-rep
 curl --fail -X MKCOL -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-repo"
 
 cd repo
-find . type -f -exec curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T {} "${WEBDAV_URL}/tmp-repo/"{} \;
+find . -type f -exec curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T {} "${WEBDAV_URL}/tmp-repo/"{} \;
 
 curl -X DELETE -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/repo"
 curl --fail -X MOVE -H "Authorization: Bearer ${WEBDAV_TOKEN}" -H "Destination:${WEBDAV_URL}/repo" "${WEBDAV_URL}/tmp-repo"
