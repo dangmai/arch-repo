412bdde4770636503f8d159ae1767e1fd7b715f3
Dang Mai <contact@dangmai.net>
2020-03-14 16:30:33 -0400

    Fix relative path

--- upload.sh
+++ upload.sh
@@ -7,7 +7,8 @@ set -euxo pipefail
 curl -X DELETE -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-repo"
 curl --fail -X MKCOL -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/tmp-repo"
 
-for file in repo/*
+cd repo
+for file in ./*
 do
   curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T ${file} "${WEBDAV_URL}/tmp-repo/${file}"
 done
