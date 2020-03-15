93cfd6180608cc948b65fe5af61aa072b2c63315
Dang Mai <contact@dangmai.net>
2020-03-14 21:43:57 -0400

    Remove debug logs

--- upload.sh
+++ upload.sh
@@ -7,5 +7,4 @@ set -euxo pipefail
 curl -X MKCOL -H "Authorization: Bearer ${WEBDAV_TOKEN}" "${WEBDAV_URL}/repo"
 
 cd repo
-ls
 find . \( -type l -o -type f \) -exec curl --fail -H "Authorization: Bearer ${WEBDAV_TOKEN}" -T "{}" "${WEBDAV_URL}/repo/{}" \;
