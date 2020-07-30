e06af9ce23749ce3b6d8c531c99265b7b8bc6fc8
Dang Mai <contact@dangmai.net>
2020-07-30 13:00:40 -0400

    Skip PGP check for Aurutils

--- .github/actions/packages/entrypoint.sh
+++ .github/actions/packages/entrypoint.sh
@@ -13,13 +13,14 @@ echo "Importing GPG keys"
 # Manually getting Spotify key
 curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
 # Strip out comments before importing keys
+# Currently aurutils key cannot be imported, see: https://github.com/AladW/aurutils/issues/730
 grep -o '^[^#]*' /github/workspace/gpg_keys.txt | xargs -I '{}' gpg --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys '{}'
 
 echo "Installing aurutils"
 sudo pacman --noconfirm -Syu git cadaver
 git clone https://aur.archlinux.org/aurutils.git
 cd aurutils
-makepkg --noconfirm -si
+makepkg --noconfirm -si --skippgpcheck
 
 echo "Adding custom repository to Pacman configuration"
 cd /home/builduser
